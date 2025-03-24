6f3e49b8b7b5ee97476e300449ba88169e24eb14
abiolaah <oluwapelumi.ajuwon@gmail.com>
2025-03-24 15:55:20 -0600

    Update code to use randomly selected user to login

--- src/test/java/authentications/VerifyLoginTest.java
+++ src/test/java/authentications/VerifyLoginTest.java
@@ -2,13 +2,17 @@ package authentications;
 
 import baseTests.BaseTest;
 import com.github.javafaker.Faker;
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.*;
 import pages.AuthenticationsPage;
 
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -24,8 +28,17 @@ public class VerifyLoginTest extends BaseTest {
     @BeforeEach
     public void loadAuthData() throws Exception {
         Gson gson = new Gson();
+        Type userListType = new TypeToken<List<Map<String, Object>>>() {}.getType(); // Properly define the type
         try (FileReader reader = new FileReader("src/main/resources/data/auth_data.json")) {
-            authData = gson.fromJson(reader, Map[].class)[0]; // Load the first user from the file
+            List<Map<String, Object>> users = gson.fromJson(reader, userListType);
+
+            if (users != null && !users.isEmpty()) {
+                // Randomly select a user
+                Random random = new Random();
+                authData = users.get(random.nextInt(users.size()));
+            } else {
+                throw new RuntimeException("No user data found in auth_data.json");
+            }
         }
     }
 
@@ -52,7 +65,7 @@ public class VerifyLoginTest extends BaseTest {
 
     @Test
     @DisplayName("Verify unSuccessful Login")
-    @Order(2)
+    @Order(3)
     public void verifyUnsuccessfulLogin(){
         loginPage = homePage.clickAuthNavigation();
         loginPage.setLoginEmail(faker.internet().emailAddress());
